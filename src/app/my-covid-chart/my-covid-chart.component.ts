816fec52f9b5665ae15bce9bafdba3c64d22b825
Divyansh kakran <divyansh.kakran@symbtechnologies.com>
2020-09-23 23:01:35 +0530

    dropdown added

--- src/app/my-covid-chart/my-covid-chart.component.ts
+++ src/app/my-covid-chart/my-covid-chart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChartDataSets, ChartOptions } from 'chart.js';
 import { Label, Color } from 'ng2-charts';
 import { MyHttpService } from './my-http-service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-my-covid-chart',
@@ -9,17 +10,18 @@ import { MyHttpService } from './my-http-service';
   styleUrls: ['./my-covid-chart.component.css']
 })
 export class MyCovidChartComponent implements OnInit {
-  covidDelhi : any;
-  covidMumbai : any;
-  activeCases : any;
+  covidDelhi: any;
+  covidMumbai: any;
+  activeCases: any;
   confirmedCases: any;
-  selectedCity = 'Delhi';
+  selectedCity = 'Maharashtra';
 
-  covidDateSeries : Array<Object>;
+  covidDateSeries: Array<Object>;
+  stateWiseData: Array<Object>;
 
-  public lineChartData =  [
+  public lineChartData = [
     { data: [], label: 'Confirmed Cases' },
-    { data: [], label: 'Active Cases' , yAxisID: 'y-axis-1' },
+    { data: [], label: 'Active Cases', yAxisID: 'y-axis-1' },
   ];
   public lineChartLabels: Label[] = [];
 
@@ -56,16 +58,17 @@ export class MyCovidChartComponent implements OnInit {
   public lineChartLegend = true;
   public lineChartType = 'line';
 
-  constructor(private http: MyHttpService) { }
+  constructor(private http: MyHttpService, private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     this.loadCovidData();
   }
-  
+
   loadCovidData() {
-    this.http.httpConnection('get' , 'https://api.covid19india.org/data.json').subscribe((res) => {
-      if(res){
+    this.http.httpConnection('get', 'https://api.covid19india.org/data.json').subscribe((res) => {
+      if (res) {
         this.covidDateSeries = res['cases_time_series'];
+        this.stateWiseData = res['statewise'];
         this.setDateTimeData();
         // this.covidDelhi = res['Delhi']['districtData']['New Delhi'];
         // this.covidMumbai = res['Maharashtra']['districtData']['Mumbai'];
@@ -77,25 +80,33 @@ export class MyCovidChartComponent implements OnInit {
       }
     })
   }
-  // onChangeCity(name){
-  //   if(name == 'Delhi'){
-  //     this.confirmedCases = this.covidDelhi.confirmed;
-  //     this.activeCases = this.covidDelhi.active;
-  //     this.selectedCity = 'Delhi'
-  //   }else{
-  //     this.confirmedCases = this.covidMumbai.confirmed;
-  //     this.activeCases = this.covidMumbai.active;
-  //     this.selectedCity = 'Mumbai'
-  //   }
-  // }
+  //getting mock data as proper data set isn't provided in covid api 
+  onChangeCity(name) {
+    if (name == 'Delhi') {
+      this.selectedCity = 'Delhi';
+      this.httpClient.get('../../assets/covidData/delhi.json').subscribe((res) => {
+        this.covidDateSeries = res['cases_time_series'];
+        this.setDateTimeData();
+      });
+    } else {
+      this.selectedCity = 'Mumbai';
+      this.httpClient.get('../../assets/covidData/maharashtra.json').subscribe((res) => {
+        this.covidDateSeries = res['cases_time_series'];
+        this.setDateTimeData();
+      });
+    }
+  }
 
-  setDateTimeData(){
+  setDateTimeData() {
     let currentMonth = new Date().getMonth() - 2;
     // filtering data by month , set data after May
-    let data = this.covidDateSeries.filter(x => new Date(x['date']).getMonth() > currentMonth );
+    let data = this.covidDateSeries.filter(x => new Date(x['date']).getMonth() > currentMonth);
     this.confirmedCases = data[data.length - 1]['totalconfirmed'];
     this.activeCases = parseInt(data[data.length - 1]['totalconfirmed']) - parseInt(data[data.length - 1]['totalrecovered'])
-    for(let i = 0 ; i < data.length ; i++){
+    this.lineChartLabels = [];
+    this.lineChartData[0].data = [];
+    this.lineChartData[1].data = [];
+    for (let i = 0; i < data.length; i++) {
       let activeCases = parseInt(data[i]['totalconfirmed']) - parseInt(data[i]['totalrecovered']);
       this.lineChartData[0].data.push(parseInt(data[i]['dailyconfirmed']));
       this.lineChartData[1].data.push(activeCases);
